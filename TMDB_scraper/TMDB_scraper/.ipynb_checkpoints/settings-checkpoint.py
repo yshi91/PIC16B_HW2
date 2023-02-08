41e703e2cd8b78095a13d3e161dfa798c02f6b6f
Yu Shi <123338811+yshi91@users.noreply.github.com>
2023-02-08 12:39:27 -0800

    Update settings-checkpoint.py

--- TMDB_scraper/TMDB_scraper/.ipynb_checkpoints/settings-checkpoint.py
+++ TMDB_scraper/TMDB_scraper/.ipynb_checkpoints/settings-checkpoint.py
@@ -15,7 +15,7 @@ NEWSPIDER_MODULE = 'TMDB_scraper.spiders'
 
 CLOSESPIDER_PAGECOUNT = 20
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'TMDB_scraper (+http://www.yourdomain.com)'
+USER_AGENT = 'TMDB_scraper (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -26,7 +26,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
